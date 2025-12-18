ac8ae3af95eb442ad74b680700d8628d931d6d6b
CeoOfGoogle1 <okdavidq@gmail.com>
2025-12-18 18:10:33 +0100

    weather, sky, fog, sounds
    super guuud stuff, next for me is cloudZss

--- Assets/Scripts/TimeOfDay.cs
+++ Assets/Scripts/TimeOfDay.cs
@@ -1,21 +1,68 @@
-using System.ComponentModel;
 using UnityEngine;
 
 public class TimeOfDay : MonoBehaviour
 {
-    public float cycleLengthInMinutes = 0.5f;
-    public float DayNumber = 0;
-    public Transform sunPosition;
-    public Light sun;
+    public float cycleLengthInMinutes = 1.0f;
     public float sunBaseIntensity = 1f;
     public float sunVariation = 1.5f;
-    public Gradient sunColor;
-    public Gradient fogColor;
-    //public AnimationCurve fogDensity;
+    public float DayNumber = 0;
+    public float transitionTime;
+    public Transform sunPosition;
+    public Light sun;
+    public Material sky;
+
+    [Header("Clear")]
+    public Gradient topClear;
+    public Gradient midClear;
+    public Gradient bottomClear;
+
+    [Header("Mist")]
+    public Gradient topMist;
+    public Gradient midMist;
+    public Gradient bottomMist;
+
+    [Header("Green")]
+    public Gradient topGreen;
+    public Gradient midGreen;
+    public Gradient bottomGreen;
+
+    [Header("Dust")]
+    public Gradient topDust;
+    public Gradient midDust;
+    public Gradient bottomDust;
+
+    [Header("Black")]
+    public Gradient topBlack;
+    public Gradient midBlack;
+    public Gradient bottomBlack;
+
+    private Color oldTopColor;
+    private Color oldMidColor;
+    private Color oldBottomColor;
     [Range(0f, 1f)]
     private float timeOfDay;
+    [Range(0f, 1f)]
+    private float cycleOfDay;
     private float timeScale = 100f;
     private float intensity;
+
+    public void SetSkyColor(Gradient topColor, Gradient midColor, Gradient bottomColor)
+    {
+        oldTopColor = sky.GetColor("_UpColor");
+        sky.SetColor("_UpColor",Color.Lerp(oldTopColor, 
+        topColor.Evaluate(cycleOfDay), 
+        Time.deltaTime / transitionTime));
+
+        oldMidColor = sky.GetColor("_MidColor");
+        sky.SetColor("_MidColor",Color.Lerp(oldMidColor, 
+        midColor.Evaluate(cycleOfDay), 
+        Time.deltaTime / transitionTime));
+
+        oldBottomColor = sky.GetColor("_DownColor");
+        sky.SetColor("_DownColor",Color.Lerp(oldBottomColor, 
+        bottomColor.Evaluate(cycleOfDay), 
+        Time.deltaTime / transitionTime));
+    }
 
     void UpdateTimeScale()
     {
@@ -26,6 +73,9 @@ public class TimeOfDay : MonoBehaviour
     {
         timeOfDay += Time.deltaTime * timeScale / 86400;
         if (timeOfDay > 1) { timeOfDay -= 1f; DayNumber++; }
+
+        float shifted = Mathf.Repeat(timeOfDay - 0.25f, 1f); // shifted timeOfDay
+        cycleOfDay = 1f - Mathf.Abs(shifted * 2f - 1f); // triangle wave
     }
 
     void RotateSun()
@@ -42,21 +92,16 @@ public class TimeOfDay : MonoBehaviour
         sun.intensity = intensity * sunVariation + sunBaseIntensity;
     }
 
-    void AdjustSunColor()
-    {
-        sun.color = sunColor.Evaluate(intensity);
-    }
-
-    // Update is called once per frame
     void Update()
     {
         UpdateTimeScale();
         UpdateTime();
         RotateSun();
-        SunIntensity();
-        //AdjustSunColor();
+        SunIntensity();;
+    }
 
-        RenderSettings.fogColor = fogColor.Evaluate(timeOfDay);
-        //RenderSettings.fogDensity = fogDensity.Evaluate(timeOfDay);
+    public float GetTimeOfDay()
+    {
+        return cycleOfDay;
     }
 }
